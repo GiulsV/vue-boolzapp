2aeb4b9b65d636007a0e26072f032bb36815a410
GiuliaV <valente.giulia93@gmail.com>
2022-05-27 17:23:38 +0200

    Layout - blocchi macrostruttura

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
 console.log("prova");
+
+
+// Milestone 1
+
+// ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+// dall’interlocutore (bianco) assegnando due classi CSS diverse
+
+// ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+// nome e immagine di ogni contatto
