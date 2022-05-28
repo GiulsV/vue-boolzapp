3722bcfed7f278b6c2ad961ed0ca71b4a48e2d4e
GiuliaV <valente.giulia93@gmail.com>
2022-05-28 22:13:00 +0200

    M1 - Creazione db contatti

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,3 @@
-console.log("prova");
 
 
 // Milestone 1
@@ -8,3 +7,61 @@ console.log("prova");
 
 // ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
 // nome e immagine di ogni contatto
+
+
+
+var app = new Vue({
+
+    el: "#app",
+
+    data: {
+
+      indexChat: 0,
+      
+      allChats: contacts = [
+
+        {
+            name: 'Michele',
+            contactMessage: 'Hai portato a spasso il cane?',
+            userMessage: 'Si',
+            contactPicture: 'img/avatar2.png'
+        },
+
+        {
+            name: "Fabio",
+            contactMessage: "Ciao come stai?",
+            userMessage: 'Bene',
+            contactPicture: 'img/avatar3.png'
+        },
+
+        {
+            name: "Samuele",
+            contactMessage: "Lo sai che ha aperto una nuova pizzeria?",
+            userMessage: "Si, ma preferirei andare al cinema",
+            contactPicture: 'img/avatar4.png'
+        },
+        {
+            name: "Alessandro L.",
+            contactMessage: "Ricordati di chiamare la nonna",
+            userMessage: "Va bene, stasera la sento",
+            contactPicture: 'img/avatar5.png'
+        },
+        {
+            name:"Claudia",
+            contactMessage: "Lo sai che ha aperto una nuova pizzeria?",
+            userMessage: "Si, ma preferirei andare al cinema",
+            contactPicture: 'img/avatar6.png'
+        },
+       ]
+    },
+
+
+     methods: {
+       openChat(index) {
+         this.indexChat = index;
+       },
+  
+    }
+  })
+
+ 
