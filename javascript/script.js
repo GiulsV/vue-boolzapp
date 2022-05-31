64bbb13c3d86fcd9a3d0477eae5d1d515dbdc93d
GiuliaV <valente.giulia93@gmail.com>
2022-05-31 16:32:44 +0200

    MS 5 - Creazione menu dropdown

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,29 @@
 // Milestone 1
-
 // ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
 // dall’interlocutore (bianco) assegnando due classi CSS diverse
 // ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
 // nome e immagine di ogni contatto
 
 // Milestone 2
-
 // ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
 // messaggi relativi al contatto attivo all’interno del pannello della conversazione
 // ● Click sul contatto mostra la conversazione del contatto cliccato
 
 // Milestone 3
-
 // ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
 // “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 // ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
 // un “ok” come risposta, che apparirà dopo 1 secondo.
+
+// Milestone 4
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+// “mar” rimangono solo Marco e Martina)
+
+// Milestone 5 - opzionale
+// ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+// permette di cancellare il messaggio selezionato
+
 
 var app = new Vue({
   el: "#app",
@@ -27,6 +34,7 @@ var app = new Vue({
     inputMessage: "",
     openClass: "",
     searchInput: "",
+    indexMessage: 0,
 
     indexChat: 0,
 
@@ -39,17 +47,20 @@ var app = new Vue({
             {
               message: 'Hai portato a spasso il cane?',
               origin: "sent",
-              hour: "21:43"
+              hour: "21:43",
+              dropdown: '',
             },
             {
               message: 'Ricordati di stendere i panni',
               origin: "received",
-              hour: "15:43"
+              hour: "15:43",
+              dropdown: '',
             },
             {
               message: 'Tutto fatto!',
               origin: "sent",
-              hour: "17:24"
+              hour: "17:24",
+              dropdown: '',
             }
           ]
         },
@@ -61,17 +72,20 @@ var app = new Vue({
            {
             message: 'Ciao come stai?',
              origin: "sent",
-             hour: "12:09"
+             hour: "12:09",
+             dropdown: '',
            },
            {
             message: 'Bene grazie! Stasera ci vediamo?',
              origin: "received",
-             hour: "23:09"
+             hour: "23:09",
+             dropdown: '',
            },
            {
             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
              origin: "sent",
-             hour: "07:56"
+             hour: "07:56",
+             dropdown: '',
            }
          ]
         },
@@ -83,17 +97,20 @@ var app = new Vue({
         {
           message: 'La Marianna va in campagna',
           origin: "sent",
-          hour: "12:19"
+          hour: "12:19",
+          dropdown: '',
         },
         {
           message: 'Sicuro di non aver sbagliato chat?',
           origin: "received",
-          hour: "14:04"
+          hour: "14:04",
+          dropdown: '',
         },
         {
           message: 'Ah scusa!',
           origin: "sent",
-          hour: "18:10"
+          hour: "18:10",
+          dropdown: '',
         }
       ]
       },
@@ -105,12 +122,14 @@ var app = new Vue({
           {
             message: 'Lo sai che ha aperto una nuova pizzeria?',
             origin: "sent",
-            hour: "12:09"
+            hour: "12:09",
+            dropdown: '',
           },
           {
             message: 'Si, ma preferirei andare al cinema',
             origin: "received",
-            hour: "08:09"
+            hour: "08:09",
+            dropdown: '',
           }
         ]
         },
@@ -122,12 +141,14 @@ var app = new Vue({
            {
             message: 'Ricordati di chiamare la nonna',
              origin: "sent",
-             hour: "05:56"
+             hour: "05:56",
+             dropdown: '',
            },
            {
             message: 'Va bene, stasera la sento',
              origin: "received",
-             hour: "18:12"
+             hour: "18:12",
+             dropdown: '',
            }
          ]
         },
@@ -139,17 +160,20 @@ var app = new Vue({
           {
             message: 'Ciao Claudia, hai novità?',
             origin: "sent",
-            hour: "12:19"
+            hour: "12:19",
+            dropdown: '',
           },
           {
             message: 'Non ancora',
             origin: "received",
-            hour: "14:04"
+            hour: "14:04",
+            dropdown: '',
           },
           {
             message: 'Nessuna nuova, buona nuova',
             origin: "sent",
-            hour: "18:10"
+            hour: "18:10",
+            dropdown: '',
           }
         ]
       },
@@ -161,12 +185,14 @@ var app = new Vue({
         {
           message: 'Fai gli auguri a Martina che è il suo compleanno!',
           origin: "sent",
-          hour: "12:19"
+          hour: "12:19",
+          dropdown: '',
         },
         {
           message: 'Grazie per avermelo ricordato, le scrivo subito!',
           origin: "received",
-          hour: "14:04"
+          hour: "14:04",
+          dropdown: '',
         }
       ]
       },
@@ -178,17 +204,20 @@ var app = new Vue({
         {
           message: 'Ciao, andiamo a mangiare la pizza stasera?',
           origin: "sent",
-          hour: "12:19"
+          hour: "12:19",
+          dropdown: '',
         },
         {
           message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
           origin: "received",
-          hour: "14:04"
+          hour: "14:04",
+          dropdown: '',
         },
         {
           message: 'OK!!',
           origin: "sent",
-          hour: "18:10"
+          hour: "18:10",
+          dropdown: '',
       }
       ],
       }
@@ -223,6 +252,7 @@ var app = new Vue({
 
             message: this.inputMessage,
             origin: "sent",
+            dropdown: false,
 
           });
           
@@ -235,6 +265,7 @@ var app = new Vue({
 
             message: 'Ok',
             origin: 'received',
+            dropdown: false,
 
 
           }), 1000);
@@ -245,9 +276,7 @@ var app = new Vue({
         
     },
 
-
-
-    // Scroll all'ultimo messaggio
+    // B - Scroll all'ultimo messaggio
 
     scrollAuto() {
   
@@ -280,6 +309,28 @@ var app = new Vue({
         }
       });
     },
+
+    // MS - 5: Dropdown Menu
+
+    dropDown(index, messages) {
+
+      let message = this.contactsList[this.indexChat].allMessages[index];
+ 
+      if (message.dropdown == false) {
+      
+       message.dropdown = true;
+
+     } else{
+
+       message.dropdown = false;
+
+     }
+    },
+
+
+  //  },
+
+
 
   },
 
